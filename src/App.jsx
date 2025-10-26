ce3f7b12366e2dea52a6e6c4498fa612a9e1a8be
Fareeda Asif <fareedaasif8130@gmail.com>
2025-10-26 07:18:26 +0500

    added app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
+import Navbar from './components/Navbar'
+import MainContent from './pages/MainContent'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <></>
+    <>
+    <Navbar/>
+    <MainContent/>
+    </>
   )
 }
 
